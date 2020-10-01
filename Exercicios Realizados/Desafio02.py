33c34ec0c7958a367e86aa71f32d002198859675
Revitte <51162772+Revitte@users.noreply.github.com>
2020-10-01 18:34:06 -0300

    primeiro commit

---
+++ Exercicios Realizados/Desafio02.py
@@ -0,0 +1,4 @@
+dia=input('Qual é o seu dia de nascimento? ')
+mes=input('Qual é o mês em que você nasceu? ')
+ano=input('Em que ano você nasceu? ')
+print('Você nasceu no dia ',dia,' de ',mes, ' em ',ano)
---
+++ Exercicios Realizados/Desafio03Chefao.py
@@ -0,0 +1,4 @@
+n1=int(input('Digite um número e tecle enter'))
+n2=int(input('Digite outro número e tecle enter'))
+print('A soma dos dois numeros resulta em: ', n1+n2)
+
